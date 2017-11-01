6c24e3a7c777512e20c3b516c8fecf8dd86ebf58
Gabriel Nieves Ponce <gabriel@spotin.io>
2017-11-01 18:06:14 -0400

    test

--- app/utils/request.js
+++ app/utils/request.js
@@ -11,7 +11,6 @@ import 'whatwg-fetch';
  * @return {object}          The parsed JSON from the request
  */
 function parseJSON(response) {
-  console.log(response.json());
   return response.json();
 }
 
@@ -23,8 +22,6 @@ function parseJSON(response) {
  * @return {object|undefined} Returns either the response, or throws an error
  */
 export function checkStatus(response) {
-  console.log('response', response);
-
   if (response.status >= 200 && response.status < 300) {
     return response;
   }
