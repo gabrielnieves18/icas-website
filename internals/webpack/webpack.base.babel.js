cac2cd66673fa5ed1f4742806fbe6bb6329da0fd
Gabriel Nieves Ponce <gabriel@spotin.io>
2017-11-04 11:30:53 -0400

    test

--- internals/webpack/webpack.base.babel.js
+++ internals/webpack/webpack.base.babel.js
@@ -74,6 +74,11 @@ module.exports = (options) => ({
         NODE_ENV: JSON.stringify(process.env.NODE_ENV),
       },
     }),
+
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+
     new webpack.NamedModulesPlugin(),
   ]),
   resolve: {
